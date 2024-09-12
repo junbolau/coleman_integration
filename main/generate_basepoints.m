1d42661f51fb5e9bc2b1e898a6bb920f3b6d7640
chrisxu3 <159728649+chrisxu3@users.noreply.github.com>
2024-09-11 19:58:31 -0700

    generate all basepoints

--- main/generate_basepoints.m
+++ main/generate_basepoints.m
@@ -20,8 +20,12 @@ common_subfield := function(K, L)
     Error("You should not reach this stage!");
 end function;
 
-// Given a number field K, a prime pp of K above p, and a number field L, return a list of primes ppp of L above p "compatible" with pp. The fields K and L are assumed to be Galois over Q.
-blah := function(K, L, pp, p)
+intrinsic CompatiblePrimes(K::FldNum, L::FldNum, pp::RngOrdIdl, p::RngIntElt) -> SeqEnum
+{
+    Given a number field K, a prime pp of K above p, and a number field L, 
+    return a list of primes ppp of L above p "compatible" with pp. 
+    The fields K and L are assumed to be Galois over Q.
+}
     // First, find the intersection M of K and L.
     MK, ML, fK, fL, fMKML := common_subfield(K, L);
     if Degree(MK) eq 1 then 
@@ -49,48 +53,87 @@ blah := function(K, L, pp, p)
     ret := [x[1] : x in Factorization(qL)];
     // printf "The primes of L above q are %o\n", ret;
     return ret;
-end function;
+end intrinsic;
+
+declare type BasicBasepoint[Basepoint];
+declare attributes BasicBasepoint: p, pp, v, tau, j_modp, ap, bp, D, K, H_O;
+declare attributes Basepoint: H, N, g, B;
 
 
-find_possible_g_level_structure := function(N, H, Fr, size_A)
-    // g such that HgA = HgA*Fr (where Fr is either the Frobenius or Verschiebung depending on things)
-    // size_A = 2 (tau = i), 3 (tau = zeta_3) or 1 (else)
+
+intrinsic MakeBasicBasepoint(p::RngIntElt, tau::FldNumElt, pp::RngOrdIdl, v::PlcNumElt, j_modp::FldFinElt, ap::RngIntElt) -> BasicBasepoint
+{Constructor}
+    assert(IsPrime(p));
+    ret := New(BasicBasepoint);
+    ret`p := p;
+    ret`H_O := Parent(tau);
+    ret`K := BaseField(ret`H_O);
+    ret`tau := (ret`K)!tau;
+    ret`pp := pp;
+    ret`v := v;
+    ret`j_modp := j_modp;
+    ret`ap := ap;
+    DD := ap*ap-4*p; ret`D, ret`bp := SquarefreeFactorization(DD);
+    return ret;
+end intrinsic;
+
+intrinsic FrobeniusMatrix(N::RngIntElt, B::BasicBasepoint) -> GrpMat
+{Returns the Frobenius matrix of the base point}
     G := GL(2, Integers(N));
+    r,s := Explode(Flat(B`tau));
+    ap := B`ap; p := B`p;
+    if (ap eq 0) or (B`tau in B`pp) then 
+        return G![ap + r/s, 1/s, -(r^2+r*s*ap+s^2*p)/s, -r/s];
+    else 
+        return G![-r/s, -1/s, (r^2+r*s*ap+s^2*p)/s, ap + r/s];
+    end if;
+end intrinsic;
+
+intrinsic FindLevelStructures(N::RngIntElt, H::GrpMat, B::BasicBasepoint) -> SeqEnum
+{Finds the possible level structures on B defined over F_p}
+    size_A := (1728 mod B`p eq B`j_modp) select 2 else ((0 mod B`p eq B`j_modp) select 3 else 1);
+    G := GL(2, Integers(N));
+    Fr := FrobeniusMatrix(N,B);
     ret := [**];
     A_matrices := [[1,0,0,1],[0,-1,1,0],[1,1,-1,0]];
     A := G!A_matrices[size_A];
     a_list := [A^(i-1) : i in [1..size_A]];
-    already_seen := [**];
-    T, phi := Transversal(G, H); //phi sends g to its coset representative
+    already_seen := {**};
+    T, phi := Transversal(G, H); //phi sends g to its a coset representative of Hg
     for g in T do
         if &or[phi(g*a) in already_seen : a in a_list] then 
             continue;
         end if;
         if &or[g*Fr*a*g^(-1) in H : a in a_list] then 
-            Append(~ret, g);
+            Append(~ret, MakeBasepoint(N,g,B));
         end if;
-        Append(~already_seen, g);
+        Include(~already_seen, g);
     end for;
-    return ret;
-end function;
-
-declare type BasicBasepoint;
-declare attributes BasicBasepoint: p, pp, v, tau;
-
-intrinsic MakeBasicBasepoint(p::RngIntElt, tau::FldNumElt, pp::RngOrdIdl, v::PlcNumElt) -> BasicBasepoint
-{Constructor}
-    assert(IsPrime(p));
-    ret := New(BasicBasepoint);
-    ret`p := p;
-    ret`tau := tau;
-    ret`pp := pp;
-    ret`v := v;
     return ret;
 end intrinsic;
 
 intrinsic Print(B::BasicBasepoint)
 {Print BasicBasepoint}
-    printf "p = %o, K = %o with Hilbert class field H_O = %o, tau = %o, tau_Fr char poly is %o, pp = %o", B`p, BaseField(Parent(B`tau)), Parent(B`tau), B`tau, DefiningPolynomial(BaseField(Parent(B`tau))), B`pp; 
+    printf "(a,b,D) = (%o,%o,%o),\nHilbert class field is %o,\nPrime above %o is %o,\ntau is %o,\nj-invariant residue class is %o mod %o\n", B`ap, B`bp, B`D, B`H_O, B`p, B`pp, B`tau, B`j_modp, B`p;
+end intrinsic;
+
+intrinsic MakeBasepoint(N::RngIntElt, g::GrpMatElt, B::BasicBasepoint) -> Basepoint
+{Constructor for Basepoint}
+    ret := New(Basepoint);
+    ret`B := B;
+    ret`N := N;
+    ret`g := g;
+    return ret;
+end intrinsic;
+
+intrinsic Parent(BB::Basepoint) -> BasicBasepoint
+{Parent of BB}
+    return BB`B;
+end intrinsic;
+
+intrinsic Print(BB:Basepoint)
+{Print Basepoint}
+    printf "BASEPOINT:\n%oLevel structure given by matrix\n%o\n", Parent(BB), BB`g;
 end intrinsic;
 
 function find_embedding(x_approx, K)
@@ -139,7 +182,7 @@ intrinsic GenerateBasicBasepoints(p::RngIntElt) -> SeqEnum
             if #H_s eq 0 then 
                 pp := Factorization(p*O_HO)[1][1];
             else 
-                F := Factorization(&+ [&meet blah(AbsoluteField(H_s[i]), AbsoluteField(H_O), pp_s[i], p) : i in [1..#H_s]]);
+                F := Factorization(&+ [&meet CompatiblePrimes(AbsoluteField(H_s[i]), AbsoluteField(H_O), pp_s[i], p) : i in [1..#H_s]]);
                 printf "The possible primes to pick is %o\n", F;
                 pp := F[1][1];
             end if;
@@ -157,7 +200,9 @@ intrinsic GenerateBasicBasepoints(p::RngIntElt) -> SeqEnum
             j_conjugates_from_tau :=  [jInvariant(upper_half_conversion(Evaluate(ta, v))) : ta in tau_conjugates];
             for z in j_conjugates_from_tau do 
                 for w in j_conjugates_arithmetic do 
-                    if Abs(z - Evaluate(w, v)) lt 1e-20 then 
+                    di := Abs(z - Evaluate(w, v));
+                    if di lt 1e-20 then 
+                        // printf "Found a match. The difference is %o\n", di; 
                         Append(~j_conjugates_arithmetic_sorted, w);
                         break;
                     end if;
@@ -168,11 +213,11 @@ intrinsic GenerateBasicBasepoints(p::RngIntElt) -> SeqEnum
             _, h := ResidueClassField(pp);
             for i in [1..#j_conjugates_arithmetic_sorted] do 
                 j_c := j_conjugates_arithmetic_sorted[i];
-                j_cbar := h(O_HO!j_c);
+                j_cbar := h(O_HO!j_c);  // the image of j_c mod pp inside F_p 
                 if j_cbar in found_j_invariants then
                     continue;
                 end if;
-                Append(~ret, MakeBasicBasepoint(p, tau_conjugates[i], pp, v));
+                Append(~ret, MakeBasicBasepoint(p, tau_conjugates[i], pp, v, j_cbar, ap));
                 Include(~found_j_invariants, j_cbar);
             end for;
         end for;
@@ -180,4 +225,10 @@ intrinsic GenerateBasicBasepoints(p::RngIntElt) -> SeqEnum
         if #found_j_invariants eq p then return ret; end if;
     end for;
     error Error("You did not find all residue classes for some reason.");
+end intrinsic;
+
+intrinsic GenerateAllBasepoints(p::RngIntElt, H::GrpMat, N::RngIntElt) -> SeqEnum
+{Generates all possible basepoints, WITH the level structure.}
+    x := GenerateBasicBasepoints(p);
+    return &cat[FindLevelStructures(N,H,B) : B in x];
 end intrinsic;
--- main/generate_basepoints.sig
+++ main/generate_basepoints.sig
@@ -1,6 +1,14 @@
 177,0
-T,BasicBasepoint,-,0
-A,BasicBasepoint,4,p,pp,v,tau
-S,MakeBasicBasepoint,Constructor,0,4,0,0,0,0,0,0,0,332,,0,0,217,,0,0,28,,0,0,148,,BasicBasepoint,-38,-38,-38,-38,-38
+S,CompatiblePrimes,"Given a number field K, a prime pp of K above p, and a number field L,  return a list of primes ppp of L above p ""compatible"" with pp.  The fields K and L are assumed to be Galois over Q",0,4,0,0,0,0,0,0,0,148,,0,0,217,,0,0,27,,0,0,27,,82,-38,-38,-38,-38,-38
+T,BasicBasepoint,Basepoint,0
+A,BasicBasepoint,10,p,pp,v,tau,j_modp,ap,bp,D,K,H_O
+A,Basepoint,4,H,N,g,B
+S,MakeBasicBasepoint,Constructor,0,6,0,0,0,0,0,0,0,148,,0,0,85,,0,0,332,,0,0,217,,0,0,28,,0,0,148,,BasicBasepoint,-38,-38,-38,-38,-38
+S,FrobeniusMatrix,Returns the Frobenius matrix of the base point,0,2,0,0,0,0,0,0,0,BasicBasepoint,,0,0,148,,178,-38,-38,-38,-38,-38
+S,FindLevelStructures,Finds the possible level structures on B defined over F_p,0,3,0,0,0,0,0,0,0,BasicBasepoint,,0,0,178,,0,0,148,,82,-38,-38,-38,-38,-38
 S,Print,Print BasicBasepoint,0,1,0,0,1,0,0,0,0,BasicBasepoint,,-38,-38,-38,-38,-38,-38
+S,MakeBasepoint,Constructor for Basepoint,0,3,0,0,0,0,0,0,0,BasicBasepoint,,0,0,180,,0,0,148,,Basepoint,-38,-38,-38,-38,-38
+S,Parent,Parent of BB,0,1,0,0,0,0,0,0,0,Basepoint,,BasicBasepoint,-38,-38,-38,-38,-38
+S,Print,Print Basepoint,0,1,0,0,1,0,0,0,0,-1,,-38,-38,-38,-38,-38,-38
 S,GenerateBasicBasepoints,Generates a list of basepoints whose j-invariants cover all residue classes mod p,0,1,0,0,0,0,0,0,0,148,,82,-38,-38,-38,-38,-38
+S,GenerateAllBasepoints,"Generates all possible basepoints, WITH the level structure",0,3,0,0,0,0,0,0,0,148,,0,0,178,,0,0,148,,82,-38,-38,-38,-38,-38
