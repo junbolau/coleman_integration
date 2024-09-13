84b5c7a33e8d0699e794946e046a1b5ff1dd2317
chrisxu3 <159728649+chrisxu3@users.noreply.github.com>
2024-09-13 14:43:59 -0700

    model_free_chabauty_coleman.m
    Added in some filler code for the main file, which gives a hint as to what we need to do. However, I anticipate the end result will be far more complicated since we need to keep track of uniformizers and what not.

--- main/model_free_chabauty_coleman.m
+++ main/model_free_chabauty_coleman.m
@@ -12,6 +12,53 @@ intrinsic ModelFreeChabautyColeman(p::RngIntElt, N::RngIntElt, gens::SeqEnum : p
     OUTPUT: 
         * the Chabauty-Coleman set X(Q_p)_1 for the modular curve X defined by gens 
 }
+    // Step 1: Load the basepoints.
+
+    // IntegralPowerSeries = BigColemanIntegral + TinyIntegralPowerSeries
 
     return {};
 end intrinsic;
+
+intrinsic IntegralPowerSeries()
+{}
+end intrinsic;
+
+intrinsic BigIntegral(basis::SeqEnum, BBx::Basepoint, BBy::Basepoint) -> SeqEnum
+{}
+    A := HeckeOperator();
+    return [TinyHeckeIntegral(f, BBy) - TinyHeckeIntegral(f, BBx) : f in basis] * ((p+1)*IdentityMatrix(g) - A)^(-1); 
+end intrinsic;
+
+intrinsic TinyHeckeIntegral(p::RngIntElt, f::RngSerElt, BB::Basepoint) -> FldPadElt
+{
+    INPUTS: 
+        * "f" -- Power series element 
+        * "BB" -- Basepoint (call this x)
+    OUTPUT:
+        * \sum \int_(x_i)^x f, where each x_i corresponds to an elliptic curve p-isogenous to f
+}
+    blah := 0; // replace it with the Hecke points
+    return &+ [TinyIntegral(f,ui,u) : ui in blah];
+end intrinsic;
+
+intrinsic TinyIntegralPowerSeries(f::RngSerElt, BB::Basepoint) -> RngSerElt
+{}
+    
+end intrinsic;
+
+intrinsic TinyIntegral(f::RngSerElt, BBx::Basepoint, BBy::Basepoint) -> FldPadElt
+{}
+end intrinsic;
+
+intrinsic Antiderivative(f::RngSerElt) -> RngSerElt
+{
+    INPUTS: 
+        * "f" -- Power series element
+    OUTPUT:
+        * An antiderivative of f, as a power series.
+}
+    t := Parent(f).1;
+    prec := Precision(Parent(f));
+    return &+[ Coefficient(f,n) * t^(n+1)/(n+1) : n in [0..prec-1]];
+end intrinsic;
+R
